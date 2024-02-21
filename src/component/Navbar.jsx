c126cacc8ef5bb3d20092f023882820fbc284c41
abc-hi <revathimohancse@gmail.com>
2024-02-21 01:49:44 -0800

    2nd update

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -16,11 +16,11 @@ const Navbar = () => {
             
             
             
-            <Link to='/' style={{ marginLeft:'120px',marginRight: '50px', color: 'black', fontSize: '18px',textDecoration: 'none',fontFamily: 'Arial', fontWeight: 'bold' }}>ALL</Link>
-                        <Link to='/fullstack' style={{ marginRight: '50px', color: 'black', fontSize: '28px',textDecoration: 'none',fontFamily: 'Arial', fontWeight: 'bold' }}>FULLSTACK DEVELOPMENT</Link>
-                        <Link to='/datascience' style={{ marginRight: '50px', color: 'black', fontSize: '28px',textDecoration: 'none',fontFamily: 'Arial', fontWeight: 'bold' }}>DATA SCIENCE</Link>
-                        <Link to='/cybersecurity' style={{ marginRight: '50px', color: 'black', fontSize: '28px',textDecoration: 'none',fontFamily: 'Arial', fontWeight: 'bold' }}>CYBER SECURITY</Link>
-                        <Link to='/career' style={{ marginRight: '50px',color: 'black', fontSize: '28px',textDecoration: 'none',fontFamily: 'Arial', fontWeight: 'bold' }}>CAREER</Link></div></div>
+            <Link to='/' style={{ marginLeft:'120px',marginRight: '50px', color: 'black', fontSize: '20px',textDecoration: 'none',fontFamily: 'Arial', fontWeight: 'bold' }}>ALL</Link>
+                        <Link to='/fullstack' style={{ marginRight: '50px', color: 'black', fontSize: '20px',textDecoration: 'none',fontFamily: 'Arial', fontWeight: 'bold' }}>FULLSTACK DEVELOPMENT</Link>
+                        <Link to='/datascience' style={{ marginRight: '50px', color: 'black', fontSize: '20px',textDecoration: 'none',fontFamily: 'Arial', fontWeight: 'bold' }}>DATA SCIENCE</Link>
+                        <Link to='/cybersecurity' style={{ marginRight: '50px', color: 'black', fontSize: '20px',textDecoration: 'none',fontFamily: 'Arial', fontWeight: 'bold' }}>CYBER SECURITY</Link>
+                        <Link to='/career' style={{ marginRight: '50px',color: 'black', fontSize: '20px',textDecoration: 'none',fontFamily: 'Arial', fontWeight: 'bold' }}>CAREER</Link></div></div>
             </nav><br />
          
         </div>
